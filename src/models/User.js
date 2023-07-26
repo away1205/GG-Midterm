78cae558080325eb544ce574977067bf59b757da
Away <agwamayatullah@gmail.com>
2023-07-27 01:45:17 +0700

    Fix e11000 issue in User model

--- src/models/User.js
+++ src/models/User.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
 const userSchema = new Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
-    default: new mongoose.Types.ObjectId(),
-    alias: 'userID',
-  },
   username: {
     type: String,
     required: [true, 'Username is required'],
