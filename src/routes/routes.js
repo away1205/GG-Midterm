5dab7d83023f6fd04e409bf841c1a7f2e39367ed
Away <agwamayatullah@gmail.com>
2023-07-28 00:43:08 +0700

    Add comments in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,13 +15,28 @@ const {
   postComment,
 } = require('../controllers/commentController');
 
+// POST / - Endpoint for posting a new video
 router.post('/', postVideo);
+
+// GET / - Endpoint for getting a list of videos
 router.get('/', getListVideos);
+
+// GET /:videoID - Endpoint for getting details of a specific video
 router.get('/:videoID', getDetailVideo);
+
+// GET /:videoID/product - Endpoint for getting a list of products associated with a video
 router.get('/:videoID/product', getListProduct);
+
+// POST /:videoID/product - Endpoint for posting a new product for a video
 router.post('/:videoID/product', postProduct);
+
+// GET /:videoID/comment - Endpoint for getting a list of comments associated with a video
 router.get('/:videoID/comment', getListComment);
+
+// POST /:videoID/comment - Endpoint for posting a new comment for a video
 router.post('/:videoID/comment', postComment);
+
+// POST /user - Endpoint for creating a new user
 router.post('/user', makeUser);
 
 module.exports = router;
