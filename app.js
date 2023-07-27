665a05e6f7665a10c3b40bd25d02ed423a134102
Away <agwamayatullah@gmail.com>
2023-07-27 22:48:40 +0700

    Add comment in app.js also delete yarn log error

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Load environment variables from the .env file (if present)
 require('dotenv').config();
 
+// Import required modules
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
@@ -7,6 +9,7 @@ const mongoose = require('mongoose');
 const router = require('./src/routes/routes');
 const DATABASE_URL = process.env.MYDATABASE_URL;
 
+// Connect to the MongoDB database using the provided URL or a default one
 mongoose
   .connect(DATABASE_URL || 'mongodb://0.0.0.0:27017/tokopedia')
   .then(() => {
@@ -16,15 +19,20 @@ mongoose
     console.log(error);
   });
 
+// Parse incoming request bodies as URL-encoded data
 app.use(express.urlencoded({ extended: true }));
+
+// Use the defined routes from the 'routes' module
 app.use(router);
 
+// Error handling middleware to handle errors in the application
 app.use((err, req, res, next) => {
   let { status = 500, message = 'Terjadi Kesalahan Dalam Server' } = err;
   console.log(message);
   res.status(status).json({ status: 'Failed', message: message });
 });
 
+// Start the server on the specified PORT from environment variables or use a default of 3000
 app.listen(PORT || 3000, () => {
   console.log(`The server is running in ${PORT}`);
 });
